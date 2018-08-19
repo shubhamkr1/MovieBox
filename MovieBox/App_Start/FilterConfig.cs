6ee04de9ef6d76d6812ec136f26698061c92603a
Shubham Kumar <shubhamkumar007@gmail.com>
2018-08-19 11:13:45 -0400

    adding authorize attr globally

--- MovieBox/App_Start/FilterConfig.cs
+++ MovieBox/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace MovieBox
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
